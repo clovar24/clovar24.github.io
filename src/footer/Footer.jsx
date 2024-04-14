298ee00ca3dd9cd6c592d5777d94ed7f71546c04
나 이재림인데 잠깐계정이름 바꾼다 <101315059+clovar24@users.noreply.github.com>
2024-04-15 01:50:44 +0900

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-     <a href="https://team-egoist.notion.site/8d564ee4df9d4a73bd3c0126a38b58d0?pvs=4"> <div>개인정보 수집 및 이용약관</div>
+     <a href="https://team-egoist.notion.site/8d564ee4df9d4a73bd3c0126a38b58d0?pvs=4"> 
+      <div>개인정보 수집 및 이용약관</div>
       <div>Contact Us 2024egoist@gmail.com</div></a>
     </div>
   );
