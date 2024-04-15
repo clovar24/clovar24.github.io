a68fce7e858e9dfe8005441aae2033af3bb448da
jllee000 <jlee000@naver.com>
2024-04-15 14:38:56 +0900

    clovar page

--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -3,6 +3,17 @@ import clovarImg from "./images/clovar.png";
 import $ from "jquery";
 
 export default function Modal() {
+  const currentDate = new Date(); // 현재 날짜
+  const targetDate = new Date("2024-06-05"); // 목표 날짜
+
+  // 두 날짜의 차이를 밀리초 단위로 계산
+  const timeDiff = targetDate.getTime() - currentDate.getTime();
+
+  // 밀리초를 일 단위로 변환
+  const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+  // 계산된 D-day를 화면에 표시할 형식으로 변환
+  const dDayText = `D-${dayDiff}`;
   const modalRef = useRef(null);
   const focusTest = useRef(null);
   useEffect(() => {
@@ -35,7 +46,7 @@ export default function Modal() {
             <img src={clovarImg} alt="about" className="img-width" />
           </div>
           <div>
-            <span className="ddate">출시 D-53!</span>
+            <span className="ddate">출시 {dDayText}</span>
             <br /> <br />
             이메일을 남겨주시면 출시 알림을 드립니다.
           </div>
